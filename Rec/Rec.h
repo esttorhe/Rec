a6a95c7615a3bfbf96a18197004c9a7ffb29d45d
esttorhe <me@estebantorr.es>
2015-05-07 16:03:46 -0600

    Initial Commit

---
+++ Rec/Rec.h
@@ -0,0 +1,19 @@
+//
+//  Rec.h
+//  Rec
+//
+//  Created by Esteban Torres on 7/5/15.
+//  Copyright (c) 2015 Esteban Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Rec.
+FOUNDATION_EXPORT double RecVersionNumber;
+
+//! Project version string for Rec.
+FOUNDATION_EXPORT const unsigned char RecVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rec/PublicHeader.h>
+
+
